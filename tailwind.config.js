02a4224ffbd95b827b9741af5669339b13aa9a8b
wibus-wee <1596355173@qq.com>
2022-12-08 12:55:53 +0800

    chore(deps): change package deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,4 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-  theme: {
-    extend: {
-      typography: ({theme}) => ({
-        pink: {
-          css: {
-            '--tw-prose-pre-bg': 'rgb(29, 31, 33)',
-          }
-        }
-      })
-    }
-  }
 }
--- type.d.ts
+++ type.d.ts
@@ -1,20 +1,3 @@
-/*
- * @FilePath: /nx-theme-tiny/type.d.ts
- * @author: Wibus
- * @Date: 2022-08-07 21:14:15
- * @LastEditors: Wibus
- * @LastEditTime: 2022-08-07 21:14:15
- * Coding With IU
- */
-/*
- * @FilePath: /ns-A-My/type.d.ts
- * @author: Wibus
- * @Date: 2022-05-15 16:18:12
- * @LastEditors: Wibus
- * @LastEditTime: 2022-05-15 16:29:54
- * Coding With IU
- */
-
 declare global {
   export interface History {
     backPath: string[]
