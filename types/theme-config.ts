38983056555f95b7199d02d1dcdcbace97cbe59d
wibus-wee <1596355173@qq.com>
2022-12-09 19:03:24 +0800

    feat(types): theme config types

--- types/theme-config.ts
+++ types/theme-config.ts
@@ -1,3 +1,72 @@
+
+
+interface Background {
+  src: {
+    light: string
+    dark: string
+  }
+  position: string
+}
+
+interface Header {
+  menu: Menu[]
+}
+
+interface Menu {
+  title: string
+  path: string
+  icon: string
+  subMenu?: Menu[]
+}
+
+interface Footer {
+  icp: {
+    text: string
+    link: string
+  }
+  content: string
+  menu: Menu[]
+}
+
+enum SidebarOrders {
+  "INFO" = "personal-info",
+  "PHOTOS" = "photos",
+  CATEGORY = "category",
+  "MOTTO" = "motto",
+  "LIKES" = "likes",
+}
+
+interface Sidebar {
+  enable: boolean
+  alwaysShow: boolean
+  photos: {
+    src: string
+    alt: string
+  }[]
+  orders: SidebarOrders[]
+}
+
+
+interface Site {
+  favicon: string
+  logoSvg: string
+  subTitle: string
+  backgrounds: {
+    background: Background
+    header: Background
+  }
+  header: Header
+  footer: Footer
+  motto: {
+    content: string
+    author: string
+  }
+  sidebar: Sidebar
+}
+interface Function { }
+
 export interface GConfig {
   name: string
+  site: Site
+  function: Function
 }
