50d29e8a09ec55ed6a78f720bc2f628423f162dd
wibus-wee <1596355173@qq.com>
2022-12-09 10:42:23 +0800

    perf(seo): use config with root store

--- components/others/SEO/index.tsx
+++ components/others/SEO/index.tsx
@@ -6,20 +6,13 @@
  * @LastEditTime: 2022-08-09 14:40:24
  * Coding With IU
  */
+import { useStore } from '@hooks/useStore'
 import merge from 'lodash-es/merge'
-import sample from 'lodash-es/sample'
 import { observer } from 'mobx-react-lite'
 import type { NextSeoProps } from 'next-seo'
 import { NextSeo } from 'next-seo'
 import type { OpenGraph } from 'next-seo/lib/types'
 import type { FC } from 'react'
-import { useSnapshot } from 'valtio'
-import appState from '../../../states/appState'
-
-// import { useInitialData, useThemeConfig } from '~/hooks/use-initial-data'
-// import { getRandomImage } from '~/utils/images'
-
-// import { useStore } from '../../../store'
 
 type SEOProps = {
   title: string
@@ -29,52 +22,33 @@ type SEOProps = {
 
 export const SEO: FC<SEOProps> = observer((props) => {
   const { title, description, openGraph, ...rest } = props
-  const { aggregate } = useSnapshot(appState) as any
-  const {
-    urls: { web_url },
-    sites,
-    user,
-  } = aggregate.aggregatedData
-  const Title = `${title} - ${sites.title}`
+  const { configStore } = useStore()
+  const userStore = configStore.getConfig().user
+
+  const Title = `${title}`
 
   return (
     <NextSeo
       {...{
         title,
-        titleTemplate: `%s - ${sites.title}`,
+        titleTemplate:title,
         openGraph: merge(
           {
             profile: {
-              username: user.name || user.username,
+              username: userStore.nickname,
             },
             type: 'article',
             locale: 'zh-cn',
-            site_name: sites.title || '',
-            description:
-              description || sites.description || '',
+            site_name: userStore.description || '',
+            description: description || userStore.description || '',
             article: {
-              authors: [user.name || (sites.name as string)],
+              authors: [userStore.nickname as string],
             },
             title: Title,
-            images: [
-              {
-                url: sample(user.avatar),
-                alt: `${title} - ${sites.title}`,
-              },
-            ],
           } as OpenGraph,
           openGraph,
         ),
-        description:
-          description ||
-          sites.description ||
-          user.introduce ||
-          '',
-        twitter: {
-          cardType: 'summary',
-          site: web_url,
-        },
-
+        description: description || userStore.description || '',
         ...rest,
       }}
     />
